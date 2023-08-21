a44ba6dc09feaa95f0a31216599201f699226360
KyoungWoong <kyy980708@gmail.com>
2023-08-21 14:47:25 +0900

    initalize

---
+++ classmentor/src/main/java/com/example/classmentor/ClassmentorApplication.java
@@ -0,0 +1,13 @@
+package com.example.classmentor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassmentorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassmentorApplication.class, args);
+	}
+
+}
