fa5d2956978b4e4aabaddacca336f3769fe1e903
KyoungWoong <kyy980708@gmail.com>
2023-10-21 14:38:57 +0900

    conflict resolve

--- classmentor/src/main/java/com/example/classmentor/domain/User.java
+++ classmentor/src/main/java/com/example/classmentor/domain/User.java
@@ -1,14 +1,52 @@
 package com.example.classmentor.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long userId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "academy_id")
+    private Academy academy;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id")
+    private Group group;
+
     private String name;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    private String loginId;
+
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String school;
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
